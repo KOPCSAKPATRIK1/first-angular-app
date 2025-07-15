dc40873a20fc23980722e1a91acf6cd4e70da81a
KOPCSAKPATRIK1 <kopcsakpatrik1@gmail.com>
2025-07-15 22:09:15 +0200

    OBJECT ADDED

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -17,19 +17,26 @@ export class User {
   imagePath = computed(() => {
     return 'assets/users/' + this.avatar()
   })
-*/
+
   @Input({required: true}) id!:string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
+  */
+
+  @Input({required: true}) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
   @Output() select = new EventEmitter<string>();
   //select = output<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
